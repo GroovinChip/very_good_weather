6539c91c447f1be225e9ba2e24407aa299686bbe
Reuben Turner <groovinchip@gmail.com>
2021-06-03 16:38:37 -0400

    Update dir structure

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:very_good_weather/api/meta_weather.dart';
 import 'package:very_good_weather/api/weather_repository.dart';
 import 'package:very_good_weather/app.dart';
-import 'package:very_good_weather/presentation/bloc_observer.dart';
+import 'package:very_good_weather/state/bloc_observer.dart';
 import 'package:very_good_weather/state/blocs/settings_bloc.dart';
 import 'package:very_good_weather/state/blocs/theme_bloc.dart';
 
