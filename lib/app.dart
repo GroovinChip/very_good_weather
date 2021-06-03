e247efd41c9e1f60767ab46a0482fca058c807bf
Reuben Turner <groovinchip@gmail.com>
2021-06-03 16:41:47 -0400

    Fix file placement

--- lib/app.dart
+++ lib/app.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:very_good_weather/api/weather_repository.dart';
 import 'package:very_good_weather/state/blocs/theme_bloc.dart';
 import 'package:very_good_weather/state/blocs/weather_bloc.dart';
-import 'package:very_good_weather/state/events/theme_state.dart';
+import 'package:very_good_weather/state/theme_state.dart';
 import 'package:very_good_weather/ui/screens/weather_screen.dart';
 
 class App extends StatelessWidget {
